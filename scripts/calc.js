5c868ec1facb1ecff21f9dfbdc18d677394aa28c
Daniel Potts <djpotts2891@gmail.com>
2023-05-26 21:07:51 +0000

    created addition, subtraction function + test func

---
+++ scripts/calc.js
@@ -0,0 +1,21 @@
+function addition(num1, num2){
+    return num1 + num2
+};
+
+function subtraction(num3, num4){
+    return num3 - num4
+};
+
+function multply(num5, num6){
+    return num5 * num6
+};
+
+function divide(num7, num8){
+    return num7 / num8
+};
+
+
+module.exports = addition;
+module.exports = subtraction;
+module.exports = multply;
+module.exports = divide;
